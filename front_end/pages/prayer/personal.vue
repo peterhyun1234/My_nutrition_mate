d504073e4667573a66262408abaf3e696985040f
HyunBin_Jeon <peterhyun1234@gmail.com>
2020-05-03 21:56:51 +0900

    add pages

---
+++ front_end/pages/prayer/personal.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app id="inspire">
+    <v-container grid-list-xl>
+      개인 기도제목
+    </v-container>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  components: {
+  },
+  data () {
+    return {
+    }
+  },
+}
+</script>
+
