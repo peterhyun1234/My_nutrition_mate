30421f61b7474f9e7e0f8b63a2525cf75013b230
HyunBin_Jeon <peterhyun1234@gmail.com>
2020-05-12 00:23:04 +0900

    delete from pre Web

--- front_end/pages/index.vue
+++ front_end/pages/index.vue
@@ -29,48 +29,6 @@
           ></v-img>
         </v-flex>
 
-
-        <v-card
-          class="pa-4 ma-3 text-center"
-          outlined
-        >
-          <v-layout 
-            row
-            align-center 
-            justify-center
-          >
-            <v-flex xs12 sm6 md4>
-              <iframe 
-                width="560" height="200" 
-                src="https://www.youtube.com/embed/kbs37nqO7G4" 
-                frameborder="0"
-                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
-                allowfullscreen
-                style="width: 100%; margin-left:auto; margin-right:auto;"
-              ></iframe>
-            </v-flex>
-            
-            <v-flex xs12 sm6 md4>
-              <v-card-text>
-                <p class="headline text--primary mt-3">
-                  아주대 CCC 웹페이지
-                </p>
-                <p class="subheading text--primary mt-3">
-                  아주대 CCC에 오신 여러분을 하나님의 이름으로 축복하고, 환영합니다 :D
-                  하나님은 당신을 사랑하시며 당신을 위한 놀라운 계획을 가지고 계십니다!
-                </p>
-                <v-btn
-                  text
-                  color="deep-purple accent-4"
-                  href="https://www.youtube.com/user/soontvccc"
-                >
-                  <strong>다른 영상 보기</strong>
-                </v-btn>
-              </v-card-text>
-            </v-flex>
-          </v-layout>
-        </v-card>
-
         <v-card
           class="pa-4 ma-3 text-left"
           outlined
@@ -113,29 +71,29 @@
                     > mdi-bridge</v-icon>
                 </v-avatar>
                 <p class="headline text--primary mt-3">
-                  브릿지 카드
+                  기능 1
                 </p>
                 <p class="subheading text--primary mt-3">
-                  질문을 통해 서로를 알아가도록 돕는 카드입니다.
-                  처음 만난 사람과 대화를 이어가기 어려워하는
-                  요즘 세대들이 일상의 소소한 질문부터 깊은 영적인
-                  대화까지 나눌 수 있도록 만들어줍니다.
-                  각기 다른 49장의 질문카드 + 설명서 1장으로
-                  구성되어있습니다.
+                  기능 1에 대한 설명
+                  기능 1에 대한 설명
+                  기능 1에 대한 설명
+                  기능 1에 대한 설명
+                  기능 1에 대한 설명
+                  기능 1에 대한 설명
                   </p>
                 <v-btn
                   text
                   color="green accent-4"
                   to="/bridge/description"
                 >
-                  <strong>브릿지 설명</strong>
+                  <strong>기능 1 설명</strong>
                 </v-btn>
                 <v-btn
                   text
                   color="deep-purple accent-4"
                   to="/bridge/play"
                 >
-                  <strong>브릿지 시작</strong>
+                  <strong>기능 1 시작</strong>
                 </v-btn>
               </v-card-text>
             </v-flex>
@@ -162,22 +120,16 @@
                     > mdi-account-heart-outline</v-icon>
                 </v-avatar>
                 <p class="headline text--primary mt-3">
-                  사랑의 언어
+                  기능 2
                 </p>
                 <p class="subheading text--primary ma-0 font-weight-bold">
-                  "모든 사람은 사랑받고, 사랑하기 원하는 마음을 갖고 있다.
-                  각 사람의 생김새와 성격이 다르듯 사람마다 사랑하는 방식이 다르다.
-                  사랑하는 사람과의 관계를 원만하게 하기 위해서는 '사랑의 언어'를 이해해야 한다."</p>
-                <p class="subheading text--primary font-weight-bold">
-                  (게리 채프먼 목사, 1992)</p>
-                <p class="subheading text--primary mt-3">
-                  채프먼 박사가 연구한 사랑의 언어는 총 다섯 가지로 아래와 같습니다.</p>
-                <p class="subheading text--primary mt-3">
-                  <code>인정하는 말</code>/
-                  <code>함께하는 시간</code>/
-                  <code>스킨십</code>/
-                  <code>봉사(헌신)</code>/
-                  <code>선물</code></p>
+                기능 2에 대한 설명
+                기능 2에 대한 설명
+                기능 2에 대한 설명
+                기능 2에 대한 설명
+                기능 2에 대한 설명
+                기능 2에 대한 설명
+                </p>
                 <v-divider>
                 </v-divider>
                 <v-btn
@@ -185,7 +137,7 @@
                   color="deep-purple accent-4"
                   to="/langOflove/test"
                 >
-                  <strong>사랑의 언어 테스트 시작</strong>
+                  <strong>기능 2 시작</strong>
                 </v-btn>
               </v-card-text>
             </v-flex>
@@ -234,13 +186,15 @@
                     > mdi-folder-information-outline</v-icon>
                 </v-avatar>
                 <p class="headline text--primary ma-5">
-                  CCC 정보
+                  기능 3
                 </p>
                 <p class="subheading text--primary ma-3">
-                  CCC는 'Movements Everywhere'라는 비전을 가지고 
-                  성령의 능력으로 사람들에게 그리스도를 전하고 믿음을 훈련시키고, 
-                  이들이 다른 사람들을 전도하고 제자화 할 수 있도록 파송하여, 
-                  지상 명령을 성취하도록 돕는 단체입니다.</p>
+                  기능 3에 대한 설명
+                  기능 3에 대한 설명
+                  기능 3에 대한 설명
+                  기능 3에 대한 설명
+                  기능 3에 대한 설명
+                  기능 3에 대한 설명</p>
                 <v-btn
                   text
                   color="deep-purple accent-4"
@@ -263,13 +217,16 @@
                     >mdi-sprout-outline</v-icon>
                 </v-avatar>
                 <p class="headline text--primary ma-5">
-                  기도제목 공유
+                  기능 4
                 </p>
                 <p class="subheading text--primary ma-3">
-                  아주대 CCC는 사랑하는 씨맨들이 가진 여러 문제들을 놓고 
-                  우리의 중보자가 되어주시는 예수님의 이름으로 합심하여 기도합니다.  
-                  바라기는 하나님과의 지속적인 교제로 
-                  우리 씨맨들의 기도가 응답받아 우리의 삶 가운데 하나님의 살아계심이 증거되기를 소망합니다.</p>
+                  기능 4에 대한 설명기능 4에 대한 설명
+                  기능 4에 대한 설명
+                  기능 4에 대한 설명
+                  기능 4에 대한 설명
+                  기능 4에 대한 설명
+                  기능 4에 대한 설명
+                  기능 4에 대한 설명</p>
                 <v-btn
                   text
                   color="deep-purple accent-4"
@@ -292,13 +249,17 @@
                     >mdi-email-send-outline</v-icon>
                 </v-avatar>
                 <p class="headline text--primary ma-5">
-                  리더십에게 건의
+                  기능 5
                 </p>
                 <p class="subheading text--primary ma-3">
-                  아주대 CCC는 간사님과 리더십 순장님들의 노력만으로 부족합니다.
-                  모든 아주대 씨맨들이 같은 마음으로 사역하기를 원하며
-                  지속적인 피드백을 적극적으로 건의해주시기 바랍니다. 
-                  그리고 신앙적인 궁금증도 익명으로도 질문하실 수 있습니다.
+                  기능 5에 대한 설명
+                  기능 5에 대한 설명
+                  기능 5에 대한 설명
+                  기능 5에 대한 설명
+                  기능 5에 대한 설명
+                  기능 5에 대한 설명
+                  기능 5에 대한 설명
+                  기능 5에 대한 설명
                 </p>
                 <v-btn
                   text
@@ -311,74 +272,6 @@
               <v-divider>
               </v-divider>
             </v-flex>
-          </v-layout>
-        </v-card>
-
-        <v-card
-          class="pa-4 mt-3 mx-3 mb-8"
-          outlined
-        >
-          <v-layout 
-            row
-          >
-            
-            <v-flex
-              xs12 sm6 md4
-              v-for="(item,i) in otherAppsItems"
-              :key="i"
-            >
-              <v-card>
-
-                <v-list-item 
-                  three-line>
-
-                  <v-list-item-avatar
-                    class="ma-1 pa-0"
-                    size="80"
-                    tile
-                  >
-                   <v-img :src="item.src"></v-img> 
-                  </v-list-item-avatar>
-
-                  <v-list-item-content
-                    class="text-center"
-                  >
-                    <v-list-item-title class=" mb-1">{{item.title}}</v-list-item-title>
-
-                    <v-list-item-subtitle 
-                      v-if="item.android">
-                      <v-btn 
-                        icon 
-                        :href="item.android"
-                      >
-                        <v-icon>mdi-android</v-icon>
-                      </v-btn>
-                      <v-btn 
-                        icon 
-                        :href="item.ios"
-                      >
-                        <v-icon>mdi-apple</v-icon>
-                      </v-btn>
-
-                    </v-list-item-subtitle>
-                    
-                    <v-list-item-subtitle v-else>
-                      <v-btn 
-                        icon 
-                        :href="item.href"
-                      >
-                        <v-icon>mdi-instagram</v-icon>
-                        바로가기
-                      </v-btn>
-
-                    </v-list-item-subtitle>
-                  </v-list-item-content>
-
-                </v-list-item>
-
-              </v-card>
-            </v-flex>
-
           </v-layout>
         </v-card>
       </v-layout>
@@ -415,25 +308,6 @@ export default {
         {
           src: 'https://i.imgur.com/slW2DVU.png',
         },
-      ],
-      otherAppsItems: [
-        {
-          src: 'https://i.imgur.com/lQFjIgH.png', 
-          title: "순장노트 다운로드", 
-          android: "https://play.google.com/store/apps/details?id=org.kccc.sj", 
-          ios: "itms-services://?action=download-manifest&url=https://dl.dropboxusercontent.com/s/ylream78zwpf0qa/soonjang_note.plist",
-        },
-        {
-          src: 'https://i.imgur.com/3KpYGE0.png', 
-          title: "Gateway 다운로드", 
-          android: "https://play.google.com/store/apps/details?id=org.kccc.gateway", 
-          ios: "https://itunes.apple.com/kr/app/gateway/id1164352459?mt=8", 
-        },
-        {
-          src: 'https://i.imgur.com/78KTHI1.png', 
-          title: "아주대CCC 인스타그램", 
-          href: "https://www.instagram.com/ajou_ccc/", 
-        },        
       ],
       windowSize: {
         x: 0,
