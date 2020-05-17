8b0ba8c3c54929c6a85df86294f005899471b588
HyunBin_Jeon <peterhyun1234@gmail.com>
2020-05-17 21:17:44 +0900

    add some page directory

---
+++ front_end/pages/my-diet/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app id="inspire">
+    <v-container grid-list-xl>
+      내 식단 확인
+    </v-container>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  components: {
+  },
+  data () {
+    return {
+    }
+  },
+}
+</script>
+
---
+++ front_end/pages/recomendation/food.vue
@@ -0,0 +1,71 @@
+
+
+<template>
+  <v-app id="inspire">
+    <v-container class="grey lighten-5">
+      <v-row>
+        <v-col>
+          <v-card>
+            메뉴 추천 page
+          </v-card>
+        </v-col>
+        <v-col>
+          <v-card>
+            One of two columns
+          </v-card>
+        </v-col>
+        <v-col>
+          <v-card>
+            One of two columns
+          </v-card>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-card>
+            One of two columns
+          </v-card>
+        </v-col>
+        <v-col>
+          <v-card>
+            One of two columns
+          </v-card>
+        </v-col>
+        <v-col>
+          <v-card>
+            One of two columns
+          </v-card>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col
+          v-for="n in 3"
+          :key="n"
+          cols="sm"
+        >
+          <v-card
+            class="pa-2"
+            outlined
+            tile
+          >
+            col
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-app>
+</template>
+
+
+<script>
+
+export default {
+  components: {
+  },
+  data () {
+    return {
+    }
+  },
+}
+</script>
+
