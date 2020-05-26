a1c4ecb6f1069e54f040ecd47e3429753e493cd4
HyunBin_Jeon <peterhyun1234@gmail.com>
2020-05-26 17:04:34 +0900

    add sign up page

---
+++ front_end/pages/signup/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app id="inspire">
+    <v-container grid-list-xl>
+      회원가입
+    </v-container>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  components: {
+  },
+  data () {
+    return {
+    }
+  },
+}
+</script>
+
