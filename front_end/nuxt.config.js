0815c37dff58caa8fd86a4ddffabab7eff4f1dce
HyunBin_Jeon <peterhyun1234@gmail.com>
2020-05-26 17:37:03 +0900

    add firebase by npm

--- front_end/nuxt.config.js
+++ front_end/nuxt.config.js
@@ -22,6 +22,12 @@ module.exports = {
      */
 
     /*
+     ** Plugins to load before mounting the App
+     */
+    plugins: [
+        '~/plugins/firebase',
+    ],
+    /*
      ** Nuxt.js dev-modules
      */
 
