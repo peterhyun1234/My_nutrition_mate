cefd26be5180816a9360ea07e9124ee378d8c88f
HyunBin_Jeon <peterhyun1234@gmail.com>
2020-05-27 18:08:24 +0900

    add sample login

--- front_end/pages/signin/index.vue
+++ front_end/pages/signin/index.vue
@@ -1,12 +1,30 @@
 <template>
   <v-app id="inspire">
     <v-container grid-list-xl>
-      로그인
+      <h3>Login</h3>
+      <input type="text" placeholder="email"><br>
+      <input type="password" placeholder="password"><br>
+      <button>로그인</button>
+      <p>계정이 없으면 회원가입을 진행해주세요</p>
     </v-container>
   </v-app>
 </template>
 
 <script>
+  var firebaseConfig = {
+    apiKey: "AIzaSyC5zhSCHySnmcGc09IR-iATPEj9LAm1ER4",
+    authDomain: "oauthtest-265805.firebaseapp.com",
+    databaseURL: "https://oauthtest-265805.firebaseio.com",
+    projectId: "oauthtest-265805",
+    storageBucket: "oauthtest-265805.appspot.com",
+    messagingSenderId: "931210459156",
+    appId: "1:931210459156:web:b12845fd52ff2c7d4f8f01",
+    measurementId: "G-N0Q217TG2M"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
 
 export default {
   components: {
@@ -16,5 +34,7 @@ export default {
     }
   },
 }
+
+
 </script>
 
