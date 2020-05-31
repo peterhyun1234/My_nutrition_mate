e3047bd4228a62ab8e73b605e101c8835b717a69
HyunBin_Jeon <peterhyun1234@gmail.com>
2020-05-31 22:36:04 +0900

    fixing some err about firebase

--- front_end/pages/signin/index.vue
+++ front_end/pages/signin/index.vue
@@ -11,21 +11,6 @@
 </template>
 
 <script>
-  var firebaseConfig = {
-    apiKey: "AIzaSyC5zhSCHySnmcGc09IR-iATPEj9LAm1ER4",
-    authDomain: "oauthtest-265805.firebaseapp.com",
-    databaseURL: "https://oauthtest-265805.firebaseio.com",
-    projectId: "oauthtest-265805",
-    storageBucket: "oauthtest-265805.appspot.com",
-    messagingSenderId: "931210459156",
-    appId: "1:931210459156:web:b12845fd52ff2c7d4f8f01",
-    measurementId: "G-N0Q217TG2M"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
-
-
 export default {
   components: {
   },
