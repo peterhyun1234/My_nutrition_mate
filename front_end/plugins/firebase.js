440c0c0165d88c1fae0250d5fa21001d0d604cbd
HyunBin_Jeon <peterhyun1234@gmail.com>
2020-05-31 22:40:18 +0900

    아 어렵다 파베

--- front_end/plugins/firebase.js
+++ front_end/plugins/firebase.js
@@ -1,18 +1,18 @@
 import Vue from 'vue'
-import firebase from 'firebase'
+// import firebase from 'firebase'
 
-Vue.use(firebase)
+// Vue.use(firebase)
 
-var firebaseConfig = {
-    apiKey: "AIzaSyC5zhSCHySnmcGc09IR-iATPEj9LAm1ER4",
-    authDomain: "oauthtest-265805.firebaseapp.com",
-    databaseURL: "https://oauthtest-265805.firebaseio.com",
-    projectId: "oauthtest-265805",
-    storageBucket: "oauthtest-265805.appspot.com",
-    messagingSenderId: "931210459156",
-    appId: "1:931210459156:web:b12845fd52ff2c7d4f8f01",
-    measurementId: "G-N0Q217TG2M"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
+// var firebaseConfig = {
+//     apiKey: "AIzaSyC5zhSCHySnmcGc09IR-iATPEj9LAm1ER4",
+//     authDomain: "oauthtest-265805.firebaseapp.com",
+//     databaseURL: "https://oauthtest-265805.firebaseio.com",
+//     projectId: "oauthtest-265805",
+//     storageBucket: "oauthtest-265805.appspot.com",
+//     messagingSenderId: "931210459156",
+//     appId: "1:931210459156:web:b12845fd52ff2c7d4f8f01",
+//     measurementId: "G-N0Q217TG2M"
+// };
+// // Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
+// firebase.analytics();
