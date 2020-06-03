7d636e9bdd992c13e071c2d5c03fabf47f949d06
HyunBin_Jeon <peterhyun1234@gmail.com>
2020-06-03 23:02:10 +0900

    add recomendation Q

--- front_end/pages/recomendation/weeks.vue
+++ front_end/pages/recomendation/weeks.vue
@@ -6,7 +6,7 @@
       <v-row>
         <v-col>
           <v-card
-            class="mt-5 mb-10 pa-3"
+            class="mt-5 mb-5 pa-3"
             shaped
             >
             <v-layout 
@@ -44,20 +44,301 @@
           </v-card>
         </v-col>
       </v-row>
+      <v-row>
+        <v-col>
+          <v-card
+            class="mt-5 mb-5 pa-3"
+            shaped>
+            <v-card-title>
+              추가할 알러지 정보를 기입해주세요.
+            </v-card-title>
+            <v-combobox
+              v-model="model"
+              :filter="filter"
+              :hide-no-data="!search"
+              :items="items"
+              :search-input.sync="search"
+              hide-selected
+              label="Search for an option"
+              multiple
+              small-chips
+              solo
+            >
+              <template v-slot:no-data>
+                <v-list-item>
+                  <span class="subheading">Create</span>
+                  <v-chip
+                    :color="`${colors[nonce - 1]} lighten-3`"
+                    label
+                    small
+                  >
+                    {{ search }}
+                  </v-chip>
+                </v-list-item>
+              </template>
+              <template v-slot:selection="{ attrs, item, parent, selected }">
+                <v-chip
+                  v-if="item === Object(item)"
+                  v-bind="attrs"
+                  :color="`${item.color} lighten-3`"
+                  :input-value="selected"
+                  label
+                  small
+                >
+                  <span class="pr-2">
+                    {{ item.text }}
+                  </span>
+                  <v-icon
+                    small
+                    @click="parent.selectItem(item)"
+                  >mdi-close</v-icon>
+                </v-chip>
+              </template>
+              <template v-slot:item="{ index, item }">
+                <v-text-field
+                  v-if="editing === item"
+                  v-model="editing.text"
+                  autofocus
+                  flat
+                  background-color="transparent"
+                  hide-details
+                  solo
+                  @keyup.enter="edit(index, item)"
+                ></v-text-field>
+                <v-chip
+                  v-else
+                  :color="`${item.color} lighten-3`"
+                  dark
+                  label
+                  small
+                >
+                  {{ item.text }}
+                </v-chip>
+                <v-spacer></v-spacer>
+                <v-list-item-action @click.stop>
+                  <v-btn
+                    icon
+                    @click.stop.prevent="edit(index, item)"
+                  >
+                    <v-icon>{{ editing !== item ? 'mdi-pencil' : 'mdi-check' }}</v-icon>
+                  </v-btn>
+                </v-list-item-action>
+              </template>
+            </v-combobox>
+          </v-card>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-card
+            class="mt-5 mb-5 pa-3"
+            shaped>
+            <v-expansion-panels 
+              class="mb-6"
+              v-model="panel"
+              multiple
+            >
+              <v-expansion-panel
+                v-for="(item,i) in 3"
+                :key="i"
+              >
+                <v-expansion-panel-header expand-icon="mdi-menu-down" disable-icon-rotate>
+                  <template 
+                    v-slot:actions
+                    v-if="radios[i]"
+                  >
+                    <v-icon color="teal">mdi-check</v-icon>
+                  </template>
+                  <template 
+                    v-slot:actions
+                    v-else
+                  >
+                    <v-icon color="error">mdi-alert-circle</v-icon>
+                  </template>
+                </v-expansion-panel-header>
+              
+                <v-divider></v-divider>
+                <v-expansion-panel-content
+                  class= "mt-5">
+                  <v-radio-group v-model="radios[i]">
+                    <v-radio value="opt1">
+                      <template v-slot:label>
+                        <div>{{questions[i].option1}}</div>
+                      </template>
+                    </v-radio>
+                    <v-radio value="opt2">
+                      <template v-slot:label>
+                        <div>{{questions[i].option2}}</div>
+                      </template>
+                    </v-radio>
+                  </v-radio-group>
+                </v-expansion-panel-content>
+              </v-expansion-panel>
+            </v-expansion-panels>
+          </v-card>
+        </v-col>
+      </v-row>
     </v-container>
   </v-app>
 </template>
 
 
 <script>
+  export default {
+    data: () => ({
+      refreshDialog: false,
+      panel: [],
+      radios: [], 
+      questions: [
+        {
+          option1: "나는 나를 인정하는 내용의 문자나 카톡을 받는 것을 좋아한다.", 
+          option2: "나는 안기는 것이 좋다.", 
+          option1_prop: "A", 
+          option2_prop: "E", 
+        },   
+        {
+          option1: "특별한 사람과 일대일로 시간을 보내는 게 좋다.", 
+          option2: "누군가 내게 도움을 줄 때 사랑 받는다고 느낀다.", 
+          option1_prop: "B", 
+          option2_prop: "D", 
+        },   
+        {
+          option1: "사람들로부터 의미있는 선물을 받는 게 좋다.", 
+          option2: "친구들과 사랑하는 사람들을 만나 여유 있게 시간을 보내는 게 좋다.", 
+          option1_prop: "C", 
+          option2_prop: "A", 
+        },      
+      ],
 
-export default {
-  components: {
-  },
-  data () {
-    return {
-    }
-  },
-}
+
+      activator: null,
+      attach: null,
+      colors: ['green', 'purple', 'indigo', 'cyan', 'teal', 'orange'],
+      editing: null,
+      index: -1,
+      items: [
+        { header: '알러지에 대한 정보를 입력함으로써 추가하실 수 있습니다.' },
+        {
+          text: '새우',
+          color: 'blue',
+        },
+        {
+          text: '갑각류',
+          color: 'red',
+        },
+      ],
+      nonce: 1,
+      menu: false,
+      model: [
+        {
+          text: '새우',
+          color: 'blue',
+        },
+      ],
+      x: 0,
+      search: null,
+      y: 0,
+    }),
+
+    watch: {
+      model (val, prev) {
+        if (val.length === prev.length) return
+
+        this.model = val.map(v => {
+          if (typeof v === 'string') {
+            v = {
+              text: v,
+              color: this.colors[this.nonce - 1],
+            }
+
+            this.items.push(v)
+
+            this.nonce++
+          }
+
+          return v
+        })
+      },
+    },
+
+    mounted() {
+      this.init();
+    },
+
+    methods: {
+      edit (index, item) {
+        if (!this.editing) {
+          this.editing = item
+          this.index = index
+        } else {
+          this.editing = null
+          this.index = -1
+        }
+      },
+      filter (item, queryText, itemText) {
+        if (item.header) return false
+
+        const hasValue = val => val != null ? val : ''
+
+        const text = hasValue(itemText)
+        const query = hasValue(queryText)
+
+        return text.toString()
+          .toLowerCase()
+          .indexOf(query.toString().toLowerCase()) > -1
+      },
+      init () {
+        for(let i = 0; i < 3; i++){
+          this.panel.push(i);
+        }
+        
+        //console.log(this.panel);
+      },
+      refresh(){
+        this.radios = [];
+      },
+      showResult(){
+        // 하나라도 check 안한 항목이 있는지
+        if(this.checkResult()){
+          this.carculateResult();
+        }
+      },
+      checkResult(){
+        let uncheckedRadios = [];
+        for(let i = 0; i < this.panel.length; i++){
+          if(!this.radios[i]){
+            uncheckedRadios.push(i+1);
+          }
+        }
+        if(uncheckedRadios.length === 0){
+          return true;
+        }else{
+          alert("체크하지 않은 항목이 있습니다!\n:" + uncheckedRadios);
+          return false;
+        }
+      },
+      carculateResult(){
+        // initialize result for exact checking
+        // this.result["A"] = 0;
+        // this.result["B"] = 0;
+        // this.result["C"] = 0;
+        // this.result["D"] = 0;
+        // this.result["E"] = 0;
+        // //console.log(this.radios);
+        // for(let i = 0; i < this.radios.length; i++){
+        //   if(this.radios[i] === "opt1"){
+        //     this.result[this.questions[i].option1_prop]++;
+        //   }else{
+        //     this.result[this.questions[i].option2_prop]++;
+        //   }
+        // }
+        
+        // using localstorage
+        //localStorage.setItem("result", JSON.stringify(this.result));
+        // 참고: https://stackoverflow.com/questions/35664550/vue-js-redirection-to-another-page
+        //this.$router.push('./test/result');
+      },
+    },
+  }
 </script>
 
