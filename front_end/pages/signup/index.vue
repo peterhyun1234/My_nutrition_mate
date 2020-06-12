49ca888bb9ffc95528c91bbaa8f78953202a5815
HyunBin_Jeon <peterhyun1234@gmail.com>
2020-06-12 21:22:48 +0900

    add allergy ditector

--- front_end/pages/signup/index.vue
+++ front_end/pages/signup/index.vue
@@ -58,84 +58,58 @@
             </v-form>
           </v-card>
           <v-card
-            class="mt-5 mb-5 pa-3"
-          >
-            <v-card-title>
-              알러지 증상이 있는 음식을 기입해주세요.
-            </v-card-title>
-            <v-combobox
-              v-model="model"
-              :filter="filter"
-              :hide-no-data="!search"
-              :items="items"
-              :search-input.sync="search"
-              hide-selected
-              label="Search for an option"
-              multiple
-              small-chips
-              solo
-            >
-              <template v-slot:no-data>
-                <v-list-item>
-                  <span class="subheading">Create</span>
-                  <v-chip
-                    :color="`${colors[nonce - 1]} lighten-3`"
-                    label
-                    small
+              class="mt-5 mb-5 pa-3"
+              shaped>
+              <v-layout 
+                  row
+                  align-center 
+                  justify-center
                   >
-                    {{ search }}
-                  </v-chip>
-                </v-list-item>
-              </template>
-              <template v-slot:selection="{ attrs, item, parent, selected }">
-                <v-chip
-                  v-if="item === Object(item)"
-                  v-bind="attrs"
-                  :color="`${item.color} lighten-3`"
-                  :input-value="selected"
-                  label
-                  small
-                >
-                  <span class="pr-2">
-                    {{ item.text }}
-                  </span>
-                  <v-icon
-                    small
-                    @click="parent.selectItem(item)"
-                  >mdi-close</v-icon>
-                </v-chip>
-              </template>
-              <template v-slot:item="{ index, item }">
-                <v-text-field
-                  v-if="editing === item"
-                  v-model="editing.text"
-                  autofocus
-                  flat
-                  background-color="transparent"
-                  hide-details
-                  solo
-                  @keyup.enter="edit(index, item)"
-                ></v-text-field>
-                <v-chip
-                  v-else
-                  :color="`${item.color} lighten-3`"
-                  dark
-                  label
-                  small
-                >
-                  {{ item.text }}
-                </v-chip>
-                <v-spacer></v-spacer>
-                <v-list-item-action @click.stop>
-                  <v-btn
-                    icon
-                    @click.stop.prevent="edit(index, item)"
-                  >
-                    <v-icon>{{ editing !== item ? 'mdi-pencil' : 'mdi-check' }}</v-icon>
-                  </v-btn>
-                </v-list-item-action>
-              </template>
-            </v-combobox>
+                  <v-flex xs12 sm12 md12>
+                      <v-card-text>
+                      <v-avatar 
+                          color="#FFE0B2" 
+                          size="50"
+                          >
+                          <v-icon 
+                          color="#FFA726"
+                          > mdi-food</v-icon>
+                      </v-avatar>
+                      <p class="headline text--primary mt-3">
+                          알러지 반응이 있는 음식
+                      </p>
+                      <v-divider></v-divider>
+                      <p class="subtitle-1 text--primary mt-3 ">
+                          식단 추천은 <strong>회원가입 시 기입한 건강상태</strong>를 기반으로 합니다.</p>
+                      <p class="subtitle-1 text--primary mt-3 ">
+                          추가적으로 <strong>알러지 반응이 있는 음식</strong>들을 입력하면 보다 더 정교한 분석이 가능합니다!</p>
+                      </v-card-text>
+                      <v-divider></v-divider>
+                  </v-flex>
+                  <v-flex xs12 sm12 md12 class="mt-5 mb-5">
+                      <v-card-title>
+                      알러지 검색
+                          <v-spacer></v-spacer>
+                          <v-text-field
+                              v-model="search"
+                              append-icon="mdi-magnify"
+                              label="Search"
+                              single-line
+                              hide-details
+                          ></v-text-field>
+                      </v-card-title>
+                      <v-data-table
+                          v-model="selected"
+                          :headers="headers"
+                          :items="desserts"
+                          show-select
+                          :single-select="singleSelect"
+                          item-key="name"
+                          :search="search"
+                      >  
+                      </v-data-table>
+                  </v-flex>
+              </v-layout>
           </v-card>
           <v-card
             class="mt-5 mb-5 pa-3"
@@ -245,6 +219,40 @@ export default {
   },
   data () {
     return {
+
+      search: '',
+      singleSelect: false,
+      selected: [],
+      headers: [
+        {
+          text: '음식 종류',
+          align: 'start',
+          sortable: false,
+          value: 'name',
+        },
+      ],
+      desserts: [
+        { name: '난류(가금류)',},
+        { name: '우유',},
+        { name: '메밀',},
+        { name: '땅콩',},
+        { name: '대두',},
+        { name: '밀',},
+        { name: '고등어',},
+        { name: '게',},
+        { name: '새우',},
+        { name: '돼지고기',},
+        { name: '복숭아',},
+        { name: '토마토',},
+        { name: '아황산류',},
+        { name: '호두',},
+        { name: '닭고기',},
+        { name: '쇠고기',},
+        { name: '오징어',},
+        { name: '조개류(굴, 전복 홍합 포함)',},
+      ],
+
+
       Qradios: ['radio-3', 'radio-3', 'radio-3', 'radio-3', 'radio-3',],
 
       name: '',
@@ -263,80 +271,15 @@ export default {
         required: value => !!value || 'Required.',
         min: v => v.length >= 8 || 'Min 8 characters',
       },
-
-
-      activator: null,
-      attach: null,
-      colors: ['green', 'purple', 'indigo', 'cyan', 'teal', 'orange'],
-      editing: null,
-      index: -1,
-      items: [
-        { header: '알러지에 대한 정보를 입력함으로써 추가하실 수 있습니다.' },
-        {
-          text: '새우',
-          color: 'blue',
-        },
-        {
-          text: '오이',
-          color: 'red',
-        },
-      ],
-      nonce: 1,
-      menu: false,
-      model: [
-        {
-          text: '새우',
-          color: 'blue',
-        },
-      ],
-      x: 0,
-      search: null,
-      y: 0,
     }
   },
 
-  watch: {
-    model (val, prev) {
-      if (val.length === prev.length) return
-
-      this.model = val.map(v => {
-        if (typeof v === 'string') {
-          v = {
-            text: v,
-            color: this.colors[this.nonce - 1],
-          }
-
-          this.items.push(v)
-
-          this.nonce++
-        }
-
-        return v
-      })
-    },
+  updated() {
+    //console.log("updated");
+    //console.log(this.selected[0].name);
   },
+  
   methods: {
-    edit (index, item) {
-      if (!this.editing) {
-        this.editing = item
-        this.index = index
-      } else {
-        this.editing = null
-        this.index = -1
-      }
-    },
-    filter (item, queryText, itemText) {
-      if (item.header) return false
-
-      const hasValue = val => val != null ? val : ''
-
-      const text = hasValue(itemText)
-      const query = hasValue(queryText)
-
-      return text.toString()
-        .toLowerCase()
-        .indexOf(query.toString().toLowerCase()) > -1
-    },
     signUp(){
       firebase.auth().createUserWithEmailAndPassword(this.email, this.password).then(
         function(user){
